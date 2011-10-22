8e3d1c0a271fc8b57774bf4c4d4711a63d7daa42
Arleen <arleenamala@gmail.com>
2011-10-21 18:33:43 -0700

    Final commit for JavaScript Research Project Demo using Raphael.js library

--- sample4.js
+++ sample4.js
@@ -1,89 +1,93 @@
 
 var myDrawFunction = new function() 
 {
-this.seconds=0;
-this.flag=false;
-this.intervalid; 
+this.circle=null;
 this.rect = null;
+this.square=null;
+this.text=null;
 this.paper = null;
-this.setcountertimer = function ()
-{
-	this.seconds=5;
-};
-
+this.st = null;
+this.animObject=null;
 
 this.displayTools = function()
 {
 	document.getElementById('tool1').style.display="";
 	document.getElementById('tool2').style.display="";
 	document.getElementById('tool3').style.display="";
-	document.getElementById('tool4').innerText="Clear Screen";
-	this.paper = Raphael( document.getElementById('animateobject'),800,600);
+	document.getElementById('tool4').style.display="";
+	document.getElementById('tool5').style.display="";
+	this.paper = Raphael( document.getElementById('animateobject'),800,900);
+	this.st = this.paper.set();
 	
 };
 
 this.displayget = function(displayid)
 {
+	if(document.getElementById(displayid).style.display == "none")
+	{
 	document.getElementById(displayid).style.display="";
+	}
+	else
+	{
+	document.getElementById(displayid).style.display = "none";
+	}
 
 };
 
 this.clearPaper = function()
 {
    this.paper.clear();
+   this.st.clear();
 	
 };
 
 
-
-this.counter = function()
-{
-if(this.flag==true)
-{
-	    if(this.seconds==0)
-	{
-	   clearInterval(this.intervalid);
-	   //document.getElementById('txt2').innerText = this.seconds;
-	   this.updateResult();
-	   this.intervalid = setInterval("myDrawFunction.counter()",1000);
-
-     }
-	else
-	{
-   	    //document.getElementById('txt2').innerText = this.seconds;
-   	    this.seconds = this.seconds-1;
-	}
-}
-};
 this.drawCircle = function( x, y, r)
 {
  
- this.rect = this.paper.circle(x,y,r);
- this.rect.attr("fill","#ffc");
- //var r1=Raphael.matrix(350,300,40,50,60,70);
- //var r2=this.paper.circle(r1);
+ this.circle = this.paper.circle(x,y,r);
+ this.circle.attr("fill","#ffc");
+ this.st.push(this.circle);
+ 
 };
-this.drawRect = function( x1, y1, l, b, r1)
+
+
+this.drawRect = function( x1, y1, l, b)
 {
  
- this.rect = this.paper.rect(x1,y1,l,b,r1);
+ this.rect = this.paper.rect(x1,y1,l,b);
  this.rect.attr("fill","#f86");
+ this.st.push(this.rect);
 
 };
-this.drawSquare = function( x2, y2, s, r2)
+
+this.drawSquare = function( x2, y2, s)
 {
 
- this.rect = this.paper.rect(x2,y2,s,s,r2);
- this.rect.attr("fill","#3df");
+ this.square = this.paper.rect(x2,y2,s,s);
+ this.square.attr("fill","#3df");
+ this.st.push(this.square);
  
+};
+
+this.drawText = function( t1 )
+{
+
+this.text = this.paper.text(450,200,t1);
+this.text.attr("font-size",32);
+this.text.attr("fill","cornflowerblue");
+this.st.push(this.text);
+
 };
 
 this.getCircleIP = function()
 {
 var elementx = document.getElementById('x');
 var x = elementx.value;
+x = parseInt(x) + 100;
 var elementy = document.getElementById('y');
 var y = elementy.value;
+y = parseInt(y) + 100;
 var elementr = document.getElementById('r');
 var r = elementr.value;
 this.drawCircle(x,y,r);
@@ -92,56 +96,48 @@ this.drawCircle(x,y,r);
 this.getRectIP = function()
 {
 var x1=document.getElementById('x1').value;
+x1 = parseInt(x1) + 100;
 var y1=document.getElementById('y1').value;
+y1 = parseInt(y1) + 100;
 var l=document.getElementById('l').value;
 var b=document.getElementById('b').value;
-var r1=document.getElementById('r1').value;
-this.drawRect(x1,y1,l,b,r1);
+this.drawRect(x1,y1,l,b);
 };
 
 this.getSquareIP = function()
 {
-var x2=document.getElementById('x2').value;
+var x2=document.getElementById('x2').value; 
+x2 = parseInt(x2) + 100;
 var y2=document.getElementById('y2').value;
+y2 = parseInt(y2) + 100;
 var s=document.getElementById('s').value;
-var r2=document.getElementById('r2').value;
-this.drawSquare(x2,y2,s,r2);
+this.drawSquare(x2,y2,s);
 };
 
+this.getTextIp = function()
+{
 
+var t1 = document.getElementById('t1').value; 
+this.drawText(t1);
+
+};
 
 this.run = function()
 {
-//var anim = Raphael.animation({x:"600", y:"200"},2000,null);
-//this.rect.animate({x:"525"},10000,"linear",null);
 
-//this.rect = this.rect.rotate(45);
-//this.rect.transform("t100,100r45t-100,0");
-this.rect.animate({x:"300",y:"300",width:"150",height:"150",r:"100"},10000,"bounce",null);
-
+this.animObject = this.paper.raphael.animation({r:20,transform:"r60s3t100,50s-1.5r120"},10000,"bounce",null);
+this.st.forEach(this.st.animate(this.animObject.repeat(Infinity)),this);
 
 };
 
-this.delayeddisplay = function ()
-{
-     this.setcountertimer();
-     this.flag=true;
-     this.counter();
-     this.intervalid = setInterval("myDrawFunction.counter()",1000);
- };
-     
-this.updateResult = function()
-{
-//resulting animation
- 	this.setcountertimer();
- 	this.counter();
-};
 
 this.stop = function()
 {
-this.flag=false;
+
+this.st.forEach(this.st.stop(),this);
 
 };
+
 
 this.changecolor = function(id)
 {
